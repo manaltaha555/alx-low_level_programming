ad00cbf98e17f3052ff7fcb522a1be5ef3e72d41
manaltaha555 <manaltaha555@gmail.com>
2023-03-21 16:59:50 -0700

    hsh

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,28 +1,23 @@
 #include"main.h"
 /**
 *jack_bauer - print every minute of the day
-*Return: void
+*
+* Return: void
 */
 void jack_bauer(void)
 {
-	int i, j, k, l;
+	int i, j;
 
-	for (i = 0; i <= 2; i++)
+	for (i = 0; i < 24; i++)
 	{
-		for (j = 0; j <= 3; j++)
+		for (j = 0; j < 60; j++)
 		{
-			for (k = 0; k <= 5; k++)
-			{
-				for (l = 0; l <= 9; l++)
-				{
-					_putchar(i + '0');
-					_putchar(j + '0');
-					_putchar(':');
-					_putchar(k + '0');
-					_putchar(l + '0');
-					_putchar('\n');
-				}
-			}
+			_putchar("i / 10" + '0');
+			_putchar("i % 10" + '0');
+			_putchar(':');
+			_putchar("j / 10" + '0');
+			_putchar("j % 10" + '0');
+			_putchar('\n');
 		}
 	}
 }
