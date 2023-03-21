0d6cf5ba5918d1364ad4cecad9002d8295e7917e
manaltaha555 <manaltaha555@gmail.com>
2023-03-21 13:01:43 -0700

    jsjsj

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,17 @@
 #include"main.h"
-void print_alphabet()
+/*
+ * print_alphabet - Print the alphabit
+ *
+ * Return: Always return void
+ *
+ */
+void print_alphabet(void)
 {
-      char i;
-      for (i = 'a'; i <= 'z'; i++)
-      {
-	      _putchar(i);
-      }
-      _putchar('\n');
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,4 +1,9 @@
 #include"main.h"
+/**
+ * main - print alphabit
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	print_alphabet();
