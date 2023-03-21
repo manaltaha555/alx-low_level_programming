13f1525bdc674425d79697abe99fec2a6f03b8f1
manaltaha555 <manaltaha555@gmail.com>
2023-03-21 13:27:59 -0700

    hshhsh

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,11 @@
+#include"main.c"
+/**
+ * main - print the alphabet 10 times
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * print_alphabet_x10 - print the alphabet ten times
+ *
+ * Return: always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char j;
+
+	while (i <= 10)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+		i++;
+	}
+	_putchar('\n');
+}
