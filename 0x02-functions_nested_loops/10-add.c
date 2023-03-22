6654494bf13b17a3853e05f1351adad6544ba29a
manaltaha555 <manaltaha555@gmail.com>
2023-03-21 17:31:18 -0700

    hahha

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * add - add two integers and print the result
+ *
+ * @a: number1 to be added
+ *
+ * @b: number2 to be added
+ *
+ * Return: always int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/* times_table - print the 9 times tables
+ *
+ * Return: void
+ *
+ */
+void times_table(void)
+{
+	int i = 0 ;
+	int j;
+
+	
+}
