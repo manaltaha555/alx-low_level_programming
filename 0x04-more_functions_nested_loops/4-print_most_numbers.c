bf6204e0256a113750d234f836a6a7313435b159
manaltaha555 <manaltaha555@gmail.com>
2023-03-23 13:44:54 -0700

    hshsh

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,13 +7,12 @@
  */
 void print_most_numbers(void)
 {
-        int i;
-
-        for (i = 0; i <= 9; i++)
-        {
+	int i;
+	for (i = 0; i <= 9; i++)
+	{
 		if (i == 2 || i == 4)
 			continue;
-                _putchar(i + '0');
-        }
-        _putchar('\n');
+		_putchar(i + '0');
+	}
+	_putchar('\n');
 }
