fe57b6f4e683fa3003239d21474b6a60e63af4f3
manaltaha555 <manaltaha555@gmail.com>
2023-03-24 06:43:22 -0700

    hshh

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,12 +14,12 @@ void more_numbers(void)
 		for (j = 0; j <= 14; j++)
 		{
 			num = j;
-			if (j > 9)
+			if (num > 9)
 			{
 				_putchar(1 + 48);
 				num = j % 10;
 			}
-				_putchar(num + 48);
+			_putchar(num + 48);
 		}
 		_putchar('\n');
 	}
