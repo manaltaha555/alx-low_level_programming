b01bc9008df41ff5c7e29ece21c5412e4d7dc555
manaltaha555 <manaltaha555@gmail.com>
2023-03-24 08:28:34 -0700

    hshsh

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ *fizz_buzz - test for fiter out the programming job candidates
+ *Return: void
+ *
+ *
+ */
+void fizz_buzz(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+	}
+	printf("\n");
+}
