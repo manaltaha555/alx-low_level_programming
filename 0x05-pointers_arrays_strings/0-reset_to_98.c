1035eb02133ebb390baa14e49bb1c6fc24c096f6
manaltaha555 <manaltaha555@gmail.com>
2023-03-27 17:02:51 -0700

    ahahha

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include"main.h"
+/**
+ *reset_to_98 -  reset the value of the variable n to 98
+ *@n: int pointer to the value of n
+ *Return: void
+ *
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
