758a9f8c0d256c21639a7d2d68e3b64ebaa7025c
manaltaha555 <manaltaha555@gmail.com>
2023-03-27 17:12:09 -0700

    hshshh

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ *swap_int - swap th evalue of two integers
+ *@a: first integer
+ *@b: second integer
+ *Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int x;
+
+	x = *a;
+	*a = *b;
+	*b = x;
+}
