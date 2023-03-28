773e07978aa57b5b0692dca8dfa00779f477b2e5
manaltaha555 <manaltaha555@gmail.com>
2023-03-28 16:49:00 -0700

    jjsjsj

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ *_strcpy - copies the string
+ *@dest: string to be filled with another string
+ *@src: string to be copied
+ *Return: array of characters
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = -1;
+
+	do {
+		i++;
+		dest[i] = src[i];
+
+	}
+	while (src[i] != '\n')
+	return (dest);
+}
