6bce04994902530c30778bf7c2b7b402149e2783
manaltaha555 <manaltaha555@gmail.com>
2023-03-30 15:22:51 -0700

    hdhhd

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ *string_toupper - changes all the lowercase letters of a strig to uppercase
+ *@n: pointer
+ *Return: pointer
+ */
+char *string_toupper(char *n)
+{
+	int i = 0;
+
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+
+}
