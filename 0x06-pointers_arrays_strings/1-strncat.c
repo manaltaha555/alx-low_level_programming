936a2514154cdfccd55da92f23a2569d54f891a8
manaltaha555 <manaltaha555@gmail.com>
2023-03-30 13:23:08 -0700

    shshsh

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,14 +1,16 @@
 #include"main.h"
 /**
- **_strncat - concatenates the two strings
- *@dset: the first string 
- *@src: the second string
+ *_strncat - to concanitate two strings
+ *@dest: first strig
+ *@src: second string
+ *@n: number of bytes
+ *Return: dest string
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int size = 0, i;
 
-	while(dest[size])
+	while (dest[size])
 		size++; /*count the size of dest*/
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
