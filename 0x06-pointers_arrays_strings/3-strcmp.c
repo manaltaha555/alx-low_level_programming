eb9ec0c7f7040efb28666cc1d6422fc1c70f0f4f
manaltaha555 <manaltaha555@gmail.com>
2023-03-30 14:34:29 -0700

    shhshs

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,16 +7,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int equal = 0;
+	int i = 0;
 
-	while (*s1)
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (*s1 != *s2)
+		if (s1[i] != s2[i])
 		{
-		equal = ((int) *s1 - 48) - ((int) *s2 - 48);
+			return (s1[i] - s2[i]);
 		}
-		s1++;
-		s2++;
+		i++;
 	}
-	return (equal);
+	return (0);
 }
