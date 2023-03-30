e8118e4bc8a1c1d3b7fcbd4edf45e7d81f2d6061
manaltaha555 <manaltaha555@gmail.com>
2023-03-30 08:51:48 -0700

    hshsh

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,17 +10,13 @@ void print_diagonal(int n)
 	int space, postn;
 
 	if (n <= 0)
-	{
 		_putchar('\n');
-	}
 	else
 	{
 		for (postn = 1 ; postn <= n; postn++)/*lines*/
 		{
 			for (space = 1; space <= postn; space++)
-		{
-			_putchar(' ');
-		}
+				_putchar(' ');
 		_putchar(92);
 		_putchar('\n');
 		}
