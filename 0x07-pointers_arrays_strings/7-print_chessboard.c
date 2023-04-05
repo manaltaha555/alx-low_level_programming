4f3f0708d0fb7fa41694c451ea3b91d87a725a31
manaltaha555 <manaltaha555@gmail.com>
2023-04-04 19:56:42 -0700

    hshhs

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -13,10 +13,8 @@ void print_chessboard(char (*a)[8])
 	{
 		for (j = 0; j < 8; j++)
 		{
-			if (j == 7)
-				_putchar('\n');
-			else
-				_putchar(a[i][j]);
+			_putchar(a[i][j]);
 		}
+		_putchar('\n');
 	}
 }
