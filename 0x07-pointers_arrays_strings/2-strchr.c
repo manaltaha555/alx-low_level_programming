21c4ca77f45f5521e7a06db0f7470b6674215906
manaltaha555 <manaltaha555@gmail.com>
2023-04-04 17:15:01 -0700

    hshshh

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,15 +7,14 @@
  */
 char *_strchr(char *s, char c)
 {
-	int size = 0;
 	int i;
 
-	while (s[size])
-		size++;
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
+		{
 			return (s + i);
+		}
 	}
 	return ('\0');
 }
