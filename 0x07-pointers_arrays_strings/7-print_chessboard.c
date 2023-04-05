e8e43d1820fad9a8fab0538351e6d030e7ce648c
manaltaha555 <manaltaha555@gmail.com>
2023-04-04 19:49:16 -0700

    hshhs

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ *print_chessboard - prints the chessboard
+ *@a: the 2d array
+ *Return: void
+ *
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; a[i][7]; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			if (j == 7)
+				_putchar('\n');
+			else
+				_putchar(a[i][j]);
+		}
+	}
+}
