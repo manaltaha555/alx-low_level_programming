92a8321b3a561ca4e6ad89a7b684d395238c81d2
manaltaha555 <manaltaha555@gmail.com>
2023-04-04 18:40:42 -0700

    hshsh

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,12 +4,14 @@
  *@s: the string to be filled
  *@b: character to be filled with
  *@n: number of characters to be filled with
+ *Return: string
+ *
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; n > 0; i++, n--)
 	{
 		s[i] = b;
 	}
