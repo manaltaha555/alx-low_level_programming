a17b656b2152a4811eeed98937e089ad8ae63ce0
manaltaha555 <manaltaha555@gmail.com>
2023-04-06 18:07:49 -0700

    hshhsh

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,35 @@
 #include"main.h"
 /**
- *
- *
+ *_sqrt_recursion - find the square root of a variable
+ *@n: the variable
+ *@val: the square root
+ *Return: the square root
+ */
+int square(int n, int val);
+int _sqrt_recursion(int n)
+{
+	return (square(n, 1));
+}
+/**
+ *square - find a square root
+ *@n: the number to get its root
+ *@val: the root to calculated
+ *Return: the square root
  *
  */
+int square(int n, int val)
+{
+if (val * val == n)
+{
+return (val);
+}
+else if (val *  val < n)
+{
+return (square(n, val + 1));
+}
+else
+{
+return (-1);
+}
+}
 
