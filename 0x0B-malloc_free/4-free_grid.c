28f9df9349e683926af88e7660fb63c53f4fda95
manaltaha555 <manaltaha555@gmail.com>
2023-04-11 16:57:52 -0700

    hshhsh

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,10 +1,10 @@
 #include"main.h"
 #include<stdlib.h>
 /**
- *
- *
- *
- *
+ *free_grid - function free two diension array
+ *@grid: the two dimension array
+ *@height: the heigth of the array
+ *Return: void
  *
  */
 void free_grid(int **grid, int height)
