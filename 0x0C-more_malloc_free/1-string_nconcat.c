cb5bd06a97b5980427dbc1f438a973259f256037
manaltaha555 <manaltaha555@gmail.com>
2023-04-14 08:46:03 -0700

    hshshhs

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,40 @@
+#include"main.h"
+#include<stdlib.h>
+/**
+ *size - clclate the size of a string
+ *@a: the string
+ *Return: the size of a string
+ */
+unsigned int size(char *a)
+{
+	unsigned int i = 0;
+
+	while (a[i] != '\0')
+		i++;
+	return (i);
+}
+/**
+ *string_nconcat - concatenates two strings
+ *@s1: first string
+ *@s2: second string
+ *Return: pointer to a newwelly allocated memory
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *m;
+	unsigned int i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	m = malloc((size(s1) + n) + 1);
+	if (m == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		m[i] = s1[i];
+	for (j = 0; j < n; j++, i++)
+		m[i] = s2[j];
+	m[i] = '\0';
+	return (m);
+}
