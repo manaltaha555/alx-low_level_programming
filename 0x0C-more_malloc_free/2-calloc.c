03e684966ba10bc5d3b55033fa75a2944cb4b13f
manaltaha555 <manaltaha555@gmail.com>
2023-04-14 11:27:08 -0700

    hshshh

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -33,6 +33,6 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	m = malloc(sizeof(int) * nmemb);
 	if (m == 0)
 		return (NULL);
-	_memset(m, 0, sizeof(int) * nmemb);
+	m = _memset(m, 0, sizeof(int) * nmemb);
 	return (m);
 }
