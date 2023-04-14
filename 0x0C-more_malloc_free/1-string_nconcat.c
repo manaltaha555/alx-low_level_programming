ff006df902fcb05287d33a04768f4936e419d6d7
manaltaha555 <manaltaha555@gmail.com>
2023-04-14 08:52:41 -0700

    hshhshs

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,6 +33,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
 		m[i] = s1[i];
+	if (n >= size(s2))
+		n = size(s2);
 	for (j = 0; j < n; j++)
 	{
 		m[i] = s2[j];
