f626b2a941e72209312a2abf433ab56af9f84d97
manaltaha555 <manaltaha555@gmail.com>
2023-04-17 16:49:59 -0700

    hshhshsh

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
-#include"mian.h"
+#ifndef DOG_H
+#define DOG_H
+/**
+ *struct dog -  create a dog
+ *@name: string
+ *@age: float
+ *@owner: string
+ */
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+#endif
