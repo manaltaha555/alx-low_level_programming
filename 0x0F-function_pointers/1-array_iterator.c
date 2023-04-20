e1069bc9cafaaa92eba2028e1aa34a94df85e55e
manaltaha555 <manaltaha555@gmail.com>
2023-04-19 19:04:09 -0700

    hhshsh

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,12 +1,20 @@
-#include"function_pointers"
+#include"function_pointers.h"
 #include<stdlib.h>
+#include<stddef.h>
 /**
- *
- *
- *
- *
+ *array_iterator - iterate an array
+ *@size: size of the array
+ *@action: pointer to a function
+ *@array: the array of strings
+ *Return: void
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	if (action && array && size)
+	{
+		unsigned int i;
 
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
 }
