80d18d71e0def49c7c10309ca3d1a9cb59393448
manaltaha555 <manaltaha555@gmail.com>
2023-04-22 16:37:16 -0700

    _printf.c updated

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include<stdlib.h>
+#include"3-calc.h"
+/**
+ *get_op_func - pointer to a function the preform a specific function
+ *@s: the array of string
+ *Return: pointer to the function
+ */
+int (*get_op_func(char *s))(int a, int b)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	while (i < 5)
+	{
+		if (s && s[0] == ops[i].op[0] && !s[1])
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,54 @@
+#include"3-calc.h"
+/**
+ *op_add -  add two numbers
+ *@a: the first number
+ *@b: the second number
+ *Return: the addition of the two numbers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *op_sub - return the difference between two numbers
+ *@a: the first number
+ *@b: the second number
+ *Return: th edifference between the two numbers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *op_mul - return the multiplication of two numbers
+ *@a: the first number
+ *@b: the second number
+ *Return: the multiplication of two numbers
+ *
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *op_div - divise two numbers
+ *@a: the first number
+ *@b: the second number
+ *Return: division between two numbers
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *op_mod - calculate the module of two numbers
+ *@a: the first number
+ *@b: the second number
+ *Return: moduele of two numbers
+ *
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
