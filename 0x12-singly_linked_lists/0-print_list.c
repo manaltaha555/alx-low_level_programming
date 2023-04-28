92e87d262edbb44fa79db212d69e9220cad869a2
manaltaha555 <manaltaha555@gmail.com>
2023-04-27 17:32:02 -0700

    hshsh

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,6 +7,7 @@
 int _strlen(char *s)
 {
 	int i = 0;
+
 	if (!s)
 		return (0);
 	while (*s++)
@@ -27,9 +28,9 @@ size_t print_list(const list_t *h)
 {
 	size_t i = 0;
 
-	while(h)
+	while (h)
 	{
-		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str :"(nil)");
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
 		h =  h->next;
 		i++;
 	}
