9a893b2929941ffffbe165a198cff3bfbeffd105
manaltaha555 <manaltaha555@gmail.com>
2023-04-27 17:55:48 -0700

    hshsh

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,10 +16,18 @@ list_t *add_node(list_t **head, const char *str)
 	while (str[len])
 		len++;
 	new = malloc(sizeof(list_t));
-	if (!new)
+	if (!new || !new_head)
 		return (NULL);
-	new->str = strdup(str);
-	new->len = len;
+	if (str)
+	{
+		new->str = strdup(str);
+		if (!new)
+		{
+			free(new);
+			return (NULL);
+		}
+		new->len = len;
+	}
 	new->next = (*head);
 	(*head) = new;
 	return (*head);
