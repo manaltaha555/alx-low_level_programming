c8661ccf9a45f769bd857fa86f099286485fd85d
manaltaha555 <manaltaha555@gmail.com>
2023-04-27 17:29:04 -0700

    hshhsh

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,37 @@
+#include"lists.h"
+/**
+ *_strlen - find the size of the string
+ *@s: the string
+ *Return: the length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+	if (!s)
+		return (0);
+	while (*s++)
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ *print_list - prints all elements in the list and their sizes
+ *@h: pointer to the list
+ *Return: size of the list
+ *
+ *
+ *
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while(h)
+	{
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str :"(nil)");
+		h =  h->next;
+		i++;
+	}
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/README.h
@@ -0,0 +1,1 @@
+this is single linked list project
