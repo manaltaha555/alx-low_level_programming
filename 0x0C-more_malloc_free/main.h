a7ac046f2ad9aee41872e1ed605dd3aea8b6f6d0
manaltaha555 <manaltaha555@gmail.com>
2023-04-28 08:58:48 -0700

    hshshhs

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,4 +6,7 @@ char *_memset(char *s, char b, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 #endif
