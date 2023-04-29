c22131b081007bd37b3198753f466a45efb570c9
manaltaha555 <manaltaha555@gmail.com>
2023-04-29 07:02:21 -0700

    hdhdhh

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include"lists.h"
+/**
+ *print_listint -  print numbers of elements in a list
+ *@h: pointer to alist
+ *Return: the size of the list
+ *
+ *
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
