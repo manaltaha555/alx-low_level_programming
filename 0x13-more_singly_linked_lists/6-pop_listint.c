559abee31179092c373511f851cd0308bdfc308e
manaltaha555 <manaltaha555@gmail.com>
2023-05-01 11:38:27 -0700

    hshhsh

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,19 @@
+#include"lists.h"
+/**
+ *pop_listint remove the first element in the list
+ *@head: the list
+ *Return: value of the deleted element
+ *
+ */
+int pop_listint(listint_t **head)
+{
+	int n;
+	listint_t *temp;
+	if (!head)
+		return (0);
+	temp = (*head) -> next;
+	n = (*head)->n;
+	free(*head);
+	*head = temp;
+	return (n);
+}
