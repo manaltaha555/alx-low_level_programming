45a9a9fefddd07de33e50c0ffa84fec315160c5f
manaltaha555 <manaltaha555@gmail.com>
2023-05-02 13:46:30 -0700

    hshhsh

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,12 +17,14 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	node = (*head)->next;
 	if (index == 0)
 	{
-		(*head)->next = NULL;
-		free(head);
+		*head = (*head)->next;
+		free(temp);
 		return (1);
 	}
-	for (; temp && i < index; i++)
+	for (; i < index; i++)
 	{
+		if (!temp || !(temp->next))
+			return (-1);
 		if (i == index - 1)
 		{
 			temp->next = node->next;
