735f700f1a3ff7011e7bbd4d01c7229f184c4584
manaltaha555 <manaltaha555@gmail.com>
2023-05-05 10:29:03 -0700

    hshshh

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,41 +8,15 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i, j = 0, count1 = 0, size = 0, count2 = 0, num = 0;
+	unsigned int num = 0;
 
 	if (!b)
 		return (0);
-	while (b[size])
+	while (*b)
 	{
-		size++;
-	}
-	for (i = 0; i < size; i++)
-	{
-		if (b[i] < 'a' || b[i] > 'z')
-		{
-			if (b[i] == 'b')
-			{
-				count1++;
-				break;
-			}
-			else
-			{
-				count2++;
-				break;
-			}
-		}
-		j++;
-		count1++;
-		if (count1 >= 2 || count2 == 1)
-		{
-			return (0);
-		}
-	}
-	for (j = size - 1, i = 0; j >= 0; j--, i++)
-	{
-		if (b[j] == 'b')
-			continue;
-		num += (b[j] - '0') * (1 << i);
+		if (*b != '0' && *b != '1')
+			retur (0);
+		num = num * 2 + (*b++ - '0');
 	}
 	return (num);
 }
