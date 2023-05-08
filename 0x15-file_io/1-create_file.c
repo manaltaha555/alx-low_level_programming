0a5572f516d15019eb3ed2fe6cfa7af12359ed70
manaltaha555 <manaltaha555@gmail.com>
2023-05-08 11:05:42 -0700

    hshshh

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,7 +2,7 @@
 /**
  *create_file - create a file
  *@filename: name of the file
- *@text_contenr: the text to be writetn in the file
+ *@text_content: the text to be writetn in the file
  *Return: 1 or -1
  */
 int create_file(const char *filename, char *text_content)
@@ -16,12 +16,16 @@ int create_file(const char *filename, char *text_content)
 	{
 		f = creat(filename, S_IRUSR | S_IWUSR);
 		f = write(f, text_content, sizeof(text_content) - 1);
+		if (!f)
+			return (-1);
 		return (1);
 	}
 	else
 	{
 		f = write(f, text_content, sizeof(text_content) - 1);
-		return (0);
+		if (!f)
+			return (-1);
+		return (1);
 	}
 	return (-1);
 }
