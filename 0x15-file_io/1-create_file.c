148911edfad7cc42bb16ffa333c9c266716736cc
manaltaha555 <manaltaha555@gmail.com>
2023-05-08 11:02:11 -0700

    hshhsh

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include"main.h"
+/**
+ *create_file - create a file
+ *@filename: name of the file
+ *@text_contenr: the text to be writetn in the file
+ *Return: 1 or -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int f;
+
+	if (filename == NULL)
+		return (-1);
+	f = open(filename, O_WRONLY);
+	if (f == -1)
+	{
+		f = creat(filename, S_IRUSR | S_IWUSR);
+		f = write(f, text_content, sizeof(text_content) - 1);
+		return (1);
+	}
+	else
+	{
+		f = write(f, text_content, sizeof(text_content) - 1);
+		return (0);
+	}
+	return (-1);
+}
