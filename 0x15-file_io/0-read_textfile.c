4da39133d8150698b1f24ecfd0200ce122174937
manaltaha555 <manaltaha555@gmail.com>
2023-05-08 10:42:00 -0700

    hshhsh

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 /**
  *read_textfile - read text file
- *@filename name of the file
+ *@filename: name of the file
  *@letters: number of characters in the file
  *Return: nuber of characters in the file or 0 if it fails
  *
@@ -14,7 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int f;
 	char *buff;
 	unsigned long int w;
-       ssize_t t;
+	ssize_t t;
 
 	if (!filename)
 		return (0);
@@ -22,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (f == -1)
 		return (0);
 	buff = malloc(sizeof(char) * letters);
-	t = read(f, buff,letters);
+	t = read(f, buff, letters);
 	w = write(STDOUT_FILENO, buff, t);
 	free(buff);
 	close(f);
