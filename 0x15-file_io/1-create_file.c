2158d2c87bd5cf7da977b0c48847c6c2268d20cc
manaltaha555 <manaltaha555@gmail.com>
2023-05-08 11:25:28 -0700

    hshsh

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,7 +7,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int f, len = 0;
+	int f, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -16,22 +16,11 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len]; len++)
 			;
 	}
-	f = open(filename, O_RDWR | O_TRUNC);
-	if (f == -1)
-	{
-		f = creat(filename, S_IRUSR | S_IWUSR);
-		f = write(f, text_content, sizeof(text_content) - 1);
-		if (f == -1)
+	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(f, text_content, sizeof(char) * len);
+		if (f == -1 || w == -1)
 			return (-1);
 		return (1);
-	}
-	else
-	{
-		f = write(f, text_content, sizeof(char) * len);
-		if (f == -1)
-			return (-1);
-		return (1);
-	}
 	close(f);
-	return (-1);
+	return (1);
 }
