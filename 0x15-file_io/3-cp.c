eac3321af35df3c7fe2909e945e182e5af60b932
manaltaha555 <mnalth544@gmail.com>
2023-09-04 12:47:45 -0700

    hshsh\

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,7 +19,7 @@ int main(int ac, char **av)
 	from_fd = open(av[1], O_RDONLY);
 	if (from_fd == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file%s\n", av[1]), exit(98);
-	to_fd = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	to_fd = open(av[2], O_WRONLY | O_TRUNC | O_CREAT, 664);
 	if (to_fd == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n ", av[2]), exit(99);
 	while ((b = read(from_fd, buf, READ_BUF_SIZE)) > 0)
