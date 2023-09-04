ec507d8337b463672c1598b6d32798849b945c9d
manaltaha555 <mnalth544@gmail.com>
2023-09-04 09:57:09 -0700

    hshhsh

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,27 @@
+#include"main.h"
+/**
+ *read_textfile - reads text from a file
+ *@filename: pointer to the filename
+ *@letters: numbr of letters to read
+ *Return: number of bytes printed
+ *
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t bytes;
+	char buf[READ_BUF_SIZE * 8];
+
+	if (!filename || !letters)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	bytes = read(fd, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
+	close(fd);
+	return (bytes);
+
+
+
+}
