46496596e9d179c695c2261ce410154f649cd965
manaltaha555 <mnalth544@gmail.com>
2023-10-13 16:14:13 -0700

    hshshhsh

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include"lists.h"
+/**
+ *dlistint_le - function find the lenght of a double linked list
+ *@h: header of the double linked list
+ *Return: lenght of the linked list
+ *
+ *
+ *
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t size = 0;
+
+	while (h)
+	{
+		size++;
+		h = h->next;
+	}
+	return (size);
+}
