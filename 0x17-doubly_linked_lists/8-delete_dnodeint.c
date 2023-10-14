324159cb4364ccd9c0dda0727970fba86ffdc778
manaltaha555 <mnalth544@gmail.com>
2023-10-13 17:45:32 -0700

    hshshh

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -11,7 +11,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (*head == NULL)
 		return (-1);
-	for (; idx != 1; idx--)
+	for (; index != 1; index--)
 	{
 		if (node == NULL)
 			return (-1);
