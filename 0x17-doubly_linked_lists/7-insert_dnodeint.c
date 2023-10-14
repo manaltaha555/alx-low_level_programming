8a7a885bf24b2f73b79c231ade51b8003cfc4fd4
manaltaha555 <mnalth544@gmail.com>
2023-10-13 17:41:37 -0700

    hshhsh

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			return (NULL);
 	}
 	if (tmp->next == NULL)
-		return (add_dnodeint_at_end(h, n));
+		return (add_dnodeint_end(h, n));
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,34 @@
+#include"lists.h"
+/**
+ *delete_dnodeint_at_index - delete a node in given index
+ *@head: header to the double linked list
+ *@index: the index of the node to be deleted
+ *Return: 1 or -1
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *node = *head;
+
+	if (*head == NULL)
+		return (-1);
+	for (; idx != 1; idx--)
+	{
+		if (node == NULL)
+			return (-1);
+		node = node->next;
+	}
+	if (node == *head)
+	{
+		*head = node->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+	else
+	{
+		node->prev->next = node->next;
+		if (node->next == NULL)
+			node->prev->next = node->prev;
+	}
+	free(node);
+	return (1);
+}
